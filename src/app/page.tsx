559e8ba11e61d6f826e869906b3e69586c57eed6
bogbu <glutamine082442@gmail.com>
2023-10-19 07:23:01 +0900

    setting main page ( add logo )

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
-import Image from 'next/image'
 import {generalFont, newspaperFont} from "@/assets/font/font";
+import {Logo} from "@/layout/logo";
+import React from "react";
 
 export default function Home() {
     return (
-        <div className={newspaperFont.className}>
-            Main Page
-            <div className={generalFont.className}>
-                메인 페이지
+        <div className={"w-full "}>
+            <Logo/>
+            <div className={newspaperFont.className}>
+                Main Page
+                <div className={generalFont.className}>
+                    메인 페이지
+                </div>
             </div>
         </div>
+
     )
 }
