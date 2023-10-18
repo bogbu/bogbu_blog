b2bad5dcea4b11ac1a8c3925c945dcd597c86ae4
bogbu <glutamine082442@gmail.com>
2023-10-19 07:21:51 +0900

    change font

--- src/assets/font/font.ts
+++ src/assets/font/font.ts
@@ -1,11 +1,12 @@
-import {Oswald, Black_Han_Sans} from 'next/font/google';
+import {Oswald, Black_Han_Sans, DM_Serif_Display} from 'next/font/google';
 
-export const newspaperFont = Oswald({
-    weight: ['400', '700'],
+export const newspaperFont = DM_Serif_Display({
+    weight: ['400', '400'],
     style: ['normal'],
     subsets: ['latin'],
     display: 'swap',
 });
+
 export const generalFont = Black_Han_Sans({
     weight: ['400', '400'],
     style: ['normal'],
