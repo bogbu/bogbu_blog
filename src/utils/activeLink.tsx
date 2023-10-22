a31d3497216c71d85e9aa67c32a264a1eef9ad94
bogbu <glutamine082442@gmail.com>
2023-10-22 13:09:42 +0900

    change gnb active style

--- src/utils/activeLink.tsx
+++ src/utils/activeLink.tsx
@@ -1,27 +1,19 @@
 'use client'
 
-import { usePathname , useRouter } from 'next/navigation'
+import {usePathname, useRouter} from 'next/navigation'
 import React from "react";
-import {black,newspaperColor} from "@/assets/color/color";
 
-function ActiveLink({ children, href } : {children : React.ReactNode , href : string}) {
-    const path = usePathname()
+function ActiveLink({children, href}: { children: React.ReactNode, href: string }) {
+    const path = usePathname();
     const router = useRouter();
-    const style = {
-        display: "block",
-        marginRight: 10,
-        background: path === href ? `${black}` : ``,
-        color : path === href ? `${newspaperColor}` : `${black}`,
-        padding : "5px"
-    }
 
-    const handleClick = (e:any) => {
+    const handleClick = (e: any) => {
         e.preventDefault()
         router.push(href)
     }
 
     return (
-        <a href={href} onClick={handleClick} style={style}>
+        <a href={href} onClick={handleClick} className={`${path === href ? `active` : ``} gnbA`}>
             {children}
         </a>
     )
