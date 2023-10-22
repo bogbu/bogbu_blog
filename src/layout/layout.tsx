d0414cf6018e6202c2dbbc9f6ce1c125d188faef
bogbu <glutamine082442@gmail.com>
2023-10-22 11:19:07 +0900

    setting Layout components

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import {Header} from "@/components/header";
+import "./layout.scss";
+import {Gnb} from "@/components/gnb";
+import {newspaperFont} from "@/assets/font/font";
+import {Logo} from "@/layout/logo";
 
 export const Layout = ({children}: { children: React.ReactNode }) => {
     return (
-        <div className={"relative"}>
-            <Header/>
-            {children}
+        <div className={`layout ${newspaperFont.className}`}>
+            <Gnb/>
+            <div className={"contents"}>
+                <Logo/>
+                {children}
+            </div>
         </div>
     );
 }
