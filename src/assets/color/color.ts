18794259cbb712be0545ef04c3ff2e94c4de10c9
bogbu <glutamine082442@gmail.com>
2023-10-22 11:17:49 +0900

    change color ts

--- src/assets/color/color.ts
+++ src/assets/color/color.ts
@@ -1,3 +1,4 @@
-export const newspaperColor = "#ccc1bd";
-export const black = "#000";
+export const newspaperColor = "#e4d7c6";
+export const black = "#433b2e";
 export const white = "#fff";
+export const red = "#fe1e1c";
