e7c686cf365ac4b882945019a9e267602ec2d3b0
bogbu <glutamine082442@gmail.com>
2023-10-24 08:38:44 +0900

    add path box

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -3,13 +3,14 @@ import "./layout.scss";
 import {Gnb} from "@/components/gnb";
 import {newspaperFont} from "@/assets/font/font";
 import {Logo} from "@/layout/logo";
-
+import {Path} from "@/layout/path";
 export const Layout = ({children}: { children: React.ReactNode }) => {
     return (
         <div className={`layout ${newspaperFont.className}`}>
             <Gnb/>
             <div className={"contents"}>
                 <Logo/>
+                <Path/>
                 {children}
             </div>
         </div>
